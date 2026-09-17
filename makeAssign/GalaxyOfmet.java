822f3a3282f9e813e9d046fa3ad2c5b50f72b890
dew@win <nittayasit12@gmail.com>
2026-09-17 19:25:15 +0700

    add Assign

---
+++ makeAssign/GalaxyOfmet.java
@@ -0,0 +1,23 @@
+package makeAssign;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+public class GalaxyOfmet {
+    
+}
+
+/**
+ * frame 
+ */
+class frame extends JFrame {
+
+    public frame()  {
+        setSize(550,600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        
+    }
+    
+    
+}
