3012746bac61d742cfea0bfa692bff0ef126ea2b
dew@win <nittayasit12@gmail.com>
2026-09-19 19:21:14 +0700

    update class Met

--- makeAssign/GalaxyOfmet.java
+++ makeAssign/GalaxyOfmet.java
@@ -1,7 +1,12 @@
 package makeAssign;
 
 import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.File;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GalaxyOfmet {
     public static void main(String[] args) {
@@ -17,8 +22,42 @@ class Frame extends JFrame {
     public Frame()  {
         setSize(550,600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
         setLayout(new BorderLayout());
+
+        add(new Met());
+
         setVisible(true);
+    }
+    
+    
+}
+
+ class Galaxy extends JPanel {
+    // Met n =new Met();
+    public Galaxy() {
+        setLayout(null);
+        // setBackground(Color.BLACK);
+        
+       
+        
+    }
+    
+    
+}
+class Met extends JPanel {
+    String s =System.getProperty("user.dir")+File.separator+"4.png";
+    Image metImage = Toolkit.getDefaultToolkit().createImage(s);
+    public Met() {
+        
+        System.out.println(s +"");
+        
+        
+    }
+
+    @Override
+    public void paint(Graphics g) {
+       g.drawImage(metImage, 50, 50,1000,1000, this);
     }
     
     
